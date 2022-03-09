57d8a63ca74221f85ac56426fdb0ba9d5ca93a83
Kenny Parnell <k.parnell@gmail.com>
2022-03-09 11:17:06 -0500

    Add Benchmark Tests

---
+++ demo_05/fib_test.go
@@ -0,0 +1,62 @@
+package fib
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestFibRecursive(t *testing.T) {
+	tests := map[int]int{
+		0: 0,
+		1: 1,
+		2: 1,
+		3: 2,
+		4: 3,
+		5: 5,
+	}
+	for i, v := range tests {
+		if f := FibRecursive(i); f != v {
+			t.Fatalf("FibRecursive(%v) == %v : expected %v", i, f, v)
+		}
+	}
+}
+
+func TestFibCache(t *testing.T) {
+	tests := map[int]int{
+		0: 0,
+		1: 1,
+		2: 1,
+		3: 2,
+		4: 3,
+		5: 5,
+	}
+	for i, v := range tests {
+		if f := FibCache(i); f != v {
+			t.Fatalf("FibCache(%v) == %v : expected %v", i, f, v)
+		}
+	}
+}
+
+func BenchmarkFibRecursive(b *testing.B) {
+	ts := []int{1, 10, 40}
+	for _, t := range ts {
+		tc := func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				FibRecursive(t)
+			}
+		}
+		b.Run(strconv.Itoa(t), tc)
+	}
+}
+
+func BenchmarkFibCache(b *testing.B) {
+	ts := []int{1, 10, 40}
+	for _, t := range ts {
+		tc := func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				FibCache(t)
+			}
+		}
+		b.Run(strconv.Itoa(t), tc)
+	}
+}
